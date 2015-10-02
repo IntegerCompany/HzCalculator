c6ff0106e567cbe9d858ce5bd76b7d4cc62bf854
Andriy <AndriyTheProgrammer@gmail.com>
2015-10-02 13:12:44 +0300

    Rx Gain calculation and tests.

--- app/src/main/java/com/integerukraine/hzcalculator/calculations/Calculations.java
+++ app/src/main/java/com/integerukraine/hzcalculator/calculations/Calculations.java
@@ -7,18 +7,28 @@ public class Calculations {
 
     private double totalErp_dB;
     private double erpInWatts;
+    private double totalReceive_dBm;
 
 
-    public void resetCalculator() {
-        totalErp_dB = 0;
-        erpInWatts = 0;
-    }
+
 
     public void calculateTxGain(double txOutput_dBm, double txAntennaGain_dBi, double cableLosses_dB) {
         totalErp_dB = txOutput_dBm + txAntennaGain_dBi - cableLosses_dB;
         erpInWatts = Math.pow(10, (totalErp_dB - 30) / 10);
     }
 
+    public void calculateRxGain(double rxSensitivity_dBm, double rxAntennaGain_dBi, double cableLosses_dB) {
+        rxSensitivity_dBm = Math.abs(rxSensitivity_dBm);
+        totalReceive_dBm = rxSensitivity_dBm + rxAntennaGain_dBi - cableLosses_dB;
+    }
+
+
+    //=========================================================================================================
+    // Utility methods
+    public void resetCalculator() {
+        totalErp_dB = 0;
+        erpInWatts = 0;
+    }
 
     //=========================================================================================================
     // Getters
@@ -28,5 +38,9 @@ public class Calculations {
 
     public double getErpInWatts() {
         return erpInWatts;
+    }
+
+    public double getTotalReceive_dBm() {
+        return totalReceive_dBm;
     }
 }
--- app/src/test/java/com/integerukraine/hzcalculator/CalculationsTest.java
+++ app/src/test/java/com/integerukraine/hzcalculator/CalculationsTest.java
@@ -21,4 +21,14 @@ public class CalculationsTest {
         Assert.assertEquals(104.71d, calculator.getErpInWatts(), erpInWattsDelta);
     }
 
+    @Test
+    public void testRxGainCalculations() {
+        double totalReceiveDelta = 0.1d;
+
+        calculator.calculateRxGain(86d, 27d, 2.3d);
+        Assert.assertEquals(110.7d, calculator.getTotalReceive_dBm(), totalReceiveDelta);
+        calculator.calculateRxGain(-86d, 27d, 2.3d);
+        Assert.assertEquals(110.7d, calculator.getTotalReceive_dBm(), totalReceiveDelta);
+    }
+
 }
